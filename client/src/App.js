1844b1595419b07244e6c20396421817700e4d0d
abdullah salameh <abdullahsalameh66@gmail.com>
2024-03-17 02:20:28 +0300

    amking a req ro our server from the client

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,15 @@
 import './App.css';
+import ax from "axios"
+import { useEffect } from 'react';
+
 
 function App() {
+  useEffect(()=>{
+    ax.get("http://localhost:3001/posts").then((res)=>{
+      console.log(res)
+    })
+  },[])
+
   return (
     <div className="App">
       hello there
--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
 const db = require("./models")
+const cors = require("cors")
 const PORT = 3001;
 
 app.use(express.json());
-
+app.use(cors())
 //Routers
 const postRouter = require("./routes/Posts")
 app.use("/posts", postRouter)
