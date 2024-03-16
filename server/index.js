10c992a4936c9a36b5a1abffc35af422cf8c0c8f
abdullah salameh <abdullahsalameh66@gmail.com>
2024-03-17 01:54:32 +0300

    creating a posts rout and adding a post and get methods to the server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
 const db = require("./models")
-
 const PORT = 3001;
+
+app.use(express.json());
+
+//Routers
+const postRouter = require("./routes/Posts")
+app.use("/posts", postRouter)
 
 db.sequelize.sync().then(() => {
     app.listen(PORT, () => {
