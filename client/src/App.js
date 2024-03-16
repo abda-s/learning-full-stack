ef4271547b855e5a1a6c352cda81275ad724adf8
abdullah salameh <abdullahsalameh66@gmail.com>
2024-03-17 02:36:20 +0300

    adding a map fun to show the content and styling it

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,33 @@
 import './App.css';
 import ax from "axios"
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 
 function App() {
-  useEffect(()=>{
-    ax.get("http://localhost:3001/posts").then((res)=>{
-      console.log(res)
+  const [listOfPosts, setListOfPosts] = useState([])
+
+  useEffect(() => {
+    ax.get("http://localhost:3001/posts").then((res) => {
+      console.log(res.data)
+      setListOfPosts(res.data)
+
     })
-  },[])
+  }, [])
 
   return (
     <div className="App">
-      hello there
+      {listOfPosts.map((post, index) => {
+        return (
+          <div key={index} className='post'>
+            <div className='title'>{post.title}</div>
+            <div className='body'>{post.posttext}</div>
+            <div className='footer'>{post.username}</div>
+          </div>
+        );
+      })}
     </div>
   );
+
 }
 
 export default App;
