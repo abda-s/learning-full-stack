65106ef3f6473a42135320bab578fce1f4e0a612
abdullah salameh <abdullahsalameh66@gmail.com>
2024-03-18 02:31:31 +0300

    creating and styling the form

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 
+import { Formik, Form, Field, ErrorMessage } from "formik"
+
 function CreatePost() {
   return (
-    <div>CreatePost</div>
+    <div className="createPostPage">
+        {/* initialValues={} onSubmit={} validationSchema={}  */}
+        <Formik >
+            <Form className="formContainer">
+                <label>Tilte: </label>
+                <Field id="inputCreatPost" name="title" placeholder="Ex. title.... " />
+                <label>Post: </label>
+                <Field id="inputCreatPost" name="posttext" placeholder="Ex. wow.... " />
+                <label>username: </label>
+                <Field id="inputCreatPost" name="username" placeholder="Ex. 3adas.... " />
+
+                <button type='submit' ></button>
+            </Form>
+        </Formik>
+    </div>
   )
 }
 
