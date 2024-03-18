3108dec78a3166fa84a7b873fb1f555f68c893b6
abdullah salameh <abdullahsalameh66@gmail.com>
2024-03-19 01:41:33 +0300

    finished the post page

--- client/src/pages/Post.jsx
+++ client/src/pages/Post.jsx
@@ -1,23 +1,34 @@
-import React, { useEffect } from 'react'
-import {useParams} from "react-router-dom"
+import React, { useEffect, useState } from 'react'
+import { useParams } from "react-router-dom"
 import ax from 'axios';
 
 function Post() {
 
-    let {id} = useParams();
+    let { id } = useParams();
 
+    const [postObject, setPostObject] = useState({})
 
     useEffect(() => {
-        ax.get("http://localhost:3001/post").then((res) => {
-            console.log(res.data)
+        ax.get(`http://localhost:3001/posts/byId/${id}`).then((res) => {
+            console.log()
+            setPostObject(res.data)
         })
     }, [])
 
 
 
-  return (
-    <div>{id}</div>
-  )
+    return (
+        <div className="postPage">
+            <div className="leftSide">
+                <div className="post" id="individual">
+                    <div className="title"> {postObject.title} </div>
+                    <div className="body">{postObject.posttext}</div>
+                    <div className="footer">{postObject.username}</div>
+                </div>
+            </div>
+            <div className="rightSide">Comment Section</div>
+        </div>
+    )
 }
 
 export default Post
