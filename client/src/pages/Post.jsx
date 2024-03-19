71f5573fbcd3bee6982c7d64f1af11fa48a39a50
abdullah salameh <abdullahsalameh66@gmail.com>
2024-03-19 23:53:57 +0300

    making the frontend for the comments

--- client/src/pages/Post.jsx
+++ client/src/pages/Post.jsx
@@ -3,6 +3,8 @@ import { useParams } from "react-router-dom"
 import ax from 'axios';
 
 function Post() {
+    const [comments, setComments] = useState([]);
+    const [newComment, setNewComment] = useState("");
 
     let { id } = useParams();
 
@@ -10,11 +12,25 @@ function Post() {
 
     useEffect(() => {
         ax.get(`http://localhost:3001/posts/byId/${id}`).then((res) => {
-            console.log()
             setPostObject(res.data)
-        })
-    }, [])
+        });
+        ax.get(`http://localhost:3001/comments/${id}`).then((response) => {
+            setComments(response.data);
+        });
+    }, []);
 
+    const addComment = () => {
+        ax
+            .post("http://localhost:3001/comments", {
+                commentBody: newComment,
+                postId: id,
+            })
+            .then((response) => {
+                const commentToAdd = { commentBody: newComment };
+                setComments([...comments, commentToAdd]);
+                setNewComment("");
+            });
+    };
 
 
     return (
@@ -26,7 +42,29 @@ function Post() {
                     <div className="footer">{postObject.username}</div>
                 </div>
             </div>
-            <div className="rightSide">Comment Section</div>
+            <div className="rightSide">
+                <div className="addCommentContainer">
+                    <input
+                        type="text"
+                        placeholder="Comment..."
+                        autoComplete="off"
+                        value={newComment}
+                        onChange={(event) => {
+                            setNewComment(event.target.value);
+                        }}
+                    />
+                    <button onClick={addComment}> Add Comment</button>
+                </div>
+                <div className="listOfComments">
+                    {comments.map((comment, key) => {
+                        return (
+                            <div key={key} className="comment">
+                                {comment.commentBody}
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
         </div>
     )
 }
--- server/routes/Comments.js
+++ server/routes/Comments.js
@@ -4,7 +4,6 @@ const { comments } = require("../models")
 
 router.get("/:postId", async (req, res) => {
     try {
-        
         const postId = req.params.postId // get the post id from the params in the url
         const Comments = await comments.findAll({where:{PostId:postId}}) // go to the comments table and where the id is postId return all the elments
         res.json(Comments) 
