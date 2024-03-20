cc8178cdfb15cd82836d18b432c3137de647e1c4
abdullah salameh <abdullahsalameh66@gmail.com>
2024-03-20 21:27:39 +0300

    creating users table

---
+++ server/models/users.js
@@ -0,0 +1,19 @@
+module.exports = (sequelize, DataTypes) => {
+    const users = sequelize.define("users", {
+        username: {
+            type: DataTypes.STRING,
+            allowNULL: false,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNULL: false,
+        },
+    })
+    users.associate = (models) => {
+        users.hasMany(models.posts, { // add an association between the users table and the comments table 
+            onDelete : "cascade" // when a users is deleted it will delete every post associated with it  
+        })
+    }
+
+    return users;
+}
